66e3be85d833cf80eb61b614f9cce1ca740fb377
Sergei Blockiy <sergei.blockiy@gmail.com>
2023-02-10 10:59:41 +0200

    update cs / recreate prefub

--- Assets/Scripts/bombThrow.cs
+++ Assets/Scripts/bombThrow.cs
@@ -5,21 +5,27 @@ using DefaultNamespace;
 using UnityEngine;
 
 public class bombThrow : MonoBehaviour
-    
+
 {
-    [SerializeField]private float forceBomb = 5;
-    [SerializeField]private float damage =  10;
-    [SerializeField]private GameObject impcatPrefub;
-    [SerializeField]public GameObject grenadePrefab;
-    [SerializeField]private float delay = 3f;
-    [SerializeField]private float countDown;
-    [SerializeField]private bool HasExploded = false;
-    [SerializeField]private GameObject explosionEffect;
-    [SerializeField]private float radius = 5f;
-    [SerializeField]private float throwForce = 40f;
-    
-    
-    
+    [SerializeField] private float forceBomb = 5;
+    [SerializeField] private float damage = 10;
+    [SerializeField] private GameObject impcatPrefub;
+    [SerializeField] public GameObject grenadePrefab;
+    [SerializeField] private float delay = 3f;
+    [SerializeField] private float countDown;
+    [SerializeField] private bool HasExploded = false;
+    [SerializeField] private GameObject explosionEffect;
+    [SerializeField] private float radius = 5f;
+    [SerializeField] private float throwForce = 40f;
+
+    private void ThrowBomb()
+    {
+        GameObject grenade = Instantiate(grenadePrefab, transform.position, transform.rotation);
+        Rigidbody rb = grenade.GetComponent<Rigidbody>();
+        rb.useGravity = true;
+        rb.AddForce(transform.forward * throwForce);
+    }
+
     private void Start()
     {
         countDown = delay;
@@ -32,46 +38,37 @@ public class bombThrow : MonoBehaviour
             ThrowBomb();
         }
 
-        void ThrowBomb()
+
+        countDown -= Time.deltaTime;
+        if (countDown <= 0f && !HasExploded)
         {
-         GameObject grenade = Instantiate(grenadePrefab, transform.position, transform.rotation);
-           Rigidbody rb = grenade.GetComponent<Rigidbody>();
-           rb.AddForce(transform.forward * throwForce);
+            Explode();
         }
+
+        void Explode()
         {
+            Instantiate(explosionEffect, transform.position, transform.rotation);
+            Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
 
-            countDown -= Time.deltaTime;
-            if (countDown <= 0f && !HasExploded)
+            foreach (Collider nearbyObject in colliders)
             {
-                Explode();
+                Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddExplosionForce(forceBomb, transform.position, radius);
+                }
+
+                DestructibleObject dest = nearbyObject.GetComponent<DestructibleObject>();
+                if (dest != null)
+                {
+                    dest.ReceiveDamage(damage);
+                }
             }
 
-            void Explode()
             {
-                Instantiate(explosionEffect, transform.position, transform.rotation);
-                Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
-
-                foreach (Collider nearbyObject in colliders)
-                {
-                    Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
-                    if (rb != null)
-                    {
-                        rb.AddExplosionForce(forceBomb, transform.position, radius);
-                    }
-
-                    DestructibleObject dest = nearbyObject.GetComponent<DestructibleObject>();
-                   if (dest != null)
-                   {
-                       dest.ReceiveDamage(damage);
-                   }
-                   
-                }
-                {
-                    Destroy(gameObject);
-                }
-                print("boom");
+                Destroy(gameObject);
             }
-
+            print("boom");
         }
     }
 }
